80e1076a23a8b623c9fb5a4ee3c3d32c27511d20
Ezequiel Di Giacomo <ezequiel250891@hotmail.com>
2023-07-15 12:59:38 -0300

    Finalizado de modelaje

---
+++ Polimorfismo/Perro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Perro : ISonido
+    {
+        public string HacerSonido()
+        {
+            return "el perro hace Guau Guau";
+        }
+    }
+}
---
+++ Polimorfismo/Vaca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    public class Vaca : ISonido
+    {
+        public string HacerSonido()
+        {
+            return $"la Vaca hace muuuuuu";
+        }
+
+    }
+}
